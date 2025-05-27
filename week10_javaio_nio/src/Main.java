7a5d1f3512649d4e5eee5efcaebab546e35f7594
nebicod <dinhduchieu60@gmail.com>
2025-05-27 12:41:30 +0700

    week10

--- week10_javaio_nio/src/Main.java
+++ week10_javaio_nio/src/Main.java
@@ -1,5 +1,72 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+
+/**
+ * Ứng dụng giám sát log file realtime
+ * - Sử dụng Java NIO WatchService để giám sát thư mục logs/
+ * - Sử dụng Java IO BufferedReader/Writer để đọc/ghi file
+ * - Phân tích và lọc các dòng ERROR từ log files
+ */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        final int PORT = 1234;
+
+        try (
+                Selector selector = Selector.open();
+                ServerSocketChannel servertChannel = ServerSocketChannel.open()
+        ) {
+            servertChannel.configureBlocking(false);
+            servertChannel.bind(new InetSocketAddress(PORT));
+            servertChannel.register(selector, SelectionKey.OP_ACCEPT);
+
+            System.out.println("🚀 NIO Echo Server đang chạy tại cổng: " + PORT);
+
+            while (true) {
+                selector.select(); //Chờ sự kiện từ Channel
+                Set<SelectionKey> keys = selector.selectedKeys();
+                Iterator<SelectionKey> it = keys.iterator();
+
+                while (it.hasNext()) {
+                    SelectionKey key = it.next();
+                    it.remove();
+
+                    if (key.isAcceptable()) {
+                        SocketChannel clientChannel = servertChannel.accept();
+                        clientChannel.configureBlocking(false);
+                        clientChannel.register(selector, SelectionKey.OP_READ);
+                        System.out.println("🔧️ Kết nối mới " + clientChannel.getRemoteAddress());
+                    } else if (key.isReadable()) {
+                        SocketChannel clientChannel = (SocketChannel) key.channel();
+                        ByteBuffer buffer = ByteBuffer.allocate(2048);
+
+                        int bytesRead = clientChannel.read(buffer);
+                        if(bytesRead == -1) {
+                            System.out.println("🛑 Client ngắt kết nối: " + clientChannel.getRemoteAddress());
+                            clientChannel.close();
+                            key.cancel();
+                            continue;
+                        }
+                        buffer.flip();
+                        String received = new String(buffer.array(), 0, bytesRead);
+                        System.out.println("🎯 Nhận: " + received.trim());
+
+                        //Echo lại cho client
+                        buffer.rewind();
+                        clientChannel.write(buffer);
+                    }
+                }
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
